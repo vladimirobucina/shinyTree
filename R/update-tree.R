d929589a7363da234cc3b434f3006449d3ddaf37
Vladimir Obucina <44030997+vladimirobucina@users.noreply.github.com>
2020-04-27 22:01:06 +0200

    Update update-tree.R

--- R/update-tree.R
+++ R/update-tree.R
@@ -20,7 +20,7 @@ updateTree <- function(session, treeId, data=NULL) {
 
 #' @importFrom jsonify toJSON
 Rlist2json <- function(nestedList) {
-  as.character(jsonify::toJSON(get_flatList2(nestedList), auto_unbox = T))
+  as.character(jsonify::to_json(get_flatList2(nestedList), auto_unbox = T))
 }
 
 #' @importFrom stringr str_match
